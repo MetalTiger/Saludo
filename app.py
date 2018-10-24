984dd251acedf828c838cc69d050fc726a11f4b6
MetalTiger <44417489+MetalTiger@users.noreply.github.com>
2018-10-24 17:06:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hola compa!"
- 
 @app.route("/index")
 def index():
     return open("index.html").read()
- 
- 
- 
  
 if __name__ == "__main__":
     app.run()
